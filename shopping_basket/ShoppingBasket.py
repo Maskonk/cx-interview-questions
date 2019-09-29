77da6e6bf785c961e5d04024a76ab503cf594f1c
Kyle Walker <Randomer44@hotmail.com>
2019-09-29 03:12:53 +0100

    Added catalogue.json to store the items

--- shopping_basket/ShoppingBasket.py
+++ shopping_basket/ShoppingBasket.py
@@ -1,9 +1,12 @@
+import json
+
 
 class ShoppingBasket:
 
     def __init__(self):
         self.basket = {}
-        self.catalogue = {}
+        with open('Catalogue.json', 'r') as f:
+            self.catalogue = json.load(f)
 
     def add_item_to_basket(self, item):
         pass
