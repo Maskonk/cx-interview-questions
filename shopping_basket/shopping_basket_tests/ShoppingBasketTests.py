6f99c8c0605ee65714ccb78605d8578f0a471a1c
Kyle Walker <Randomer44@hotmail.com>
2019-09-29 03:23:37 +0100

    Added second test for adding multiple items

--- shopping_basket/shopping_basket_tests/ShoppingBasketTests.py
+++ shopping_basket/shopping_basket_tests/ShoppingBasketTests.py
@@ -7,6 +7,11 @@ class TestShoppingBasket(TestCase):
     def setUp(self):
         self.shopping_basket = ShoppingBasket()
 
-    def test_add_item_to_basket(self):
+    def test_add_item_to_basket_one_item(self):
         self.shopping_basket.add_item_to_basket("Baked Beans")
         self.assertEqual({"Baked Beans": 1}, self.shopping_basket.basket)
+
+    def test_add_item_to_basket_two_duplicate_items(self):
+        self.shopping_basket.add_item_to_basket("Biscuits")
+        self.shopping_basket.add_item_to_basket("Biscuits")
+        self.assertEqual({"Biscuits": 2}, self.shopping_basket.basket)
